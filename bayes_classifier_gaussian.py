4c59515911ed469af44a248d3ccbbac342dbe729
tada20001 <tada20001@gmail.com>
2023-01-25 19:06:28 +0900

    add .gitignore file

--- bayes_classifier_gaussian.py
+++ bayes_classifier_gaussian.py
@@ -2,7 +2,7 @@ from __future__ import print_function, division
 from builtins import range, input
 # Note: you may need to update your version of future 
 
-#import util
+import util
 import numpy as np
 
 print("hello startcoding")
