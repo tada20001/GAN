c0206006d9a24fd32f8bd2a96a8de808432468e9
tada20001 <tada20001@gmail.com>
2023-01-25 18:44:12 +0900

    add util.py file

---
+++ bayes_classifier_gaussian.py
@@ -0,0 +1,8 @@
+from __future__ import print_function, division
+from builtins import range, input
+# Note: you may need to update your version of future 
+
+#import util
+import numpy as np
+
+print("hello startcoding")
